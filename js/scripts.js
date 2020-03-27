65c399d376d334180f72f894a702a4b45635a74d
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-03-26 23:33:58 -0700

    debug js file, var operator line 22 had a spelling mistake.

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,8 @@ $(document).ready(function () {
     event.preventDefault();
       var number1 = parseInt($("#input1").val());
       var number2 = parseInt($("#input2").val());
-      var operation = $("input:radio[name=operator]:checked").val();
-      var result;
+      var operator = $("input:radio[name=operator]:checked").val();
+       var result;
       if (operator === "add") {
         result = add(number1, number2);
       }else if (operator === "subtract") {
